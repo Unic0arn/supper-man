a1b58e8176c0ff8392afd8fface9e1b81f36eba1
emiwes <emiwes@kth.se>
2016-03-11 10:23:45 +0100

    Included table. Remove button not working yet.

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ $(function (){
     var radarChartView = new RadarChartView(d3.select('.radarChartContainer'), model);
     var radarChartCtrl = new RadarChartCtrl(radarChartView, model);
 
-    //var tableView = new TableView(d3.select('.tableContainer'), model);
-    //var tableCtrl = new TableCtrl(tableView, model);
+    var tableView = new TableView(d3.select('.tableContainer'), model);
+    // var tableCtrl = new TableCtrl(tableView, model);
     
     var sunburstView = new SunburstView(d3.select('.sunburstContainer'), model);
     var sunburstCtrl = new SunburstCtrl(sunburstView, model);
