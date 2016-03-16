c3e93d95716c7c7a09d4fb2d607748133a95b212
emiwes <emiwes@kth.se>
2016-03-16 16:13:08 +0100

    Maria and I started a bit with user input!

---
+++ js/startOverviewCtrl.js
@@ -0,0 +1,27 @@
+var StartOverviewCtrl = function (view, model) {
+  
+  model.addObserver(this);
+  var ctrl = this;
+    
+
+  this.savePersonalData = function(savePersonalData){
+    // code to get all the input values and save to the model!
+    // Depending on bool, we either save personal data or store default data to the model!
+    if(savePersonalData){
+      console.log('Save personal data!');
+    }else{
+      console.log('save default data!');
+    }
+  };
+
+  view.container.startBtn.on('click', function(){
+    ctrl.savePersonalData(true);
+  });
+  view.container.skipBtn.on('click', function(){
+    ctrl.savePersonalData(false);
+  });
+
+  this.update = function(){
+    return;
+  };
+};
