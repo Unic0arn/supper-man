04c3e03f16d6cd72bc1d1ad07c4b6e8b164db9a0
emiwes <emiwes@kth.se>
2016-03-16 11:33:49 +0100

    Aggregate button text changes.

--- js/nutritionchartCtrl.js
+++ js/nutritionchartCtrl.js
@@ -2,6 +2,11 @@ var NutritionChartCtrl = function (view, model) {
 	var NutritionChartCtrl = this;
 	d3.select("#agg-switch").on("click", function(){
 		console.log("Pressed aggregate switch");
+		if(view.agg){
+			this.innerHTML = 'Aggregate!';
+		}else{
+			this.innerHTML = 'Divide!';
+		}
 		view.agg = !view.agg;
 		view.change();
 		model.notifyObservers("changeAgg");
