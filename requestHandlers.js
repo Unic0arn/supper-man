ed5df7f9f0cff94f815b9220536d2ca2f8a13c14
Fredrik Hallberg <saccer92@gmail.com>
2016-03-25 16:14:23 +0100

    Begin work on node implementation

---
+++ requestHandlers.js
@@ -0,0 +1,22 @@
+var exec = require("child_process").exec;
+function start(response) {
+	console.log("Request handler 'start' was called.");
+	 var content = "empty";
+
+ exec("tree", function (error, stdout, stderr) {
+   	response.writeHead(200, {"Content-Type": "text/plain"});
+	 response.write(stdout);
+	 response.end();
+ });
+
+} 
+
+function app(response) {
+	console.log("Request handler 'app' was called.");
+	 response.writeHead(200, {"Content-Type": "text/plain"});
+ 	response.write("Hello Upload");
+ 	response.end();
+}
+
+exports.start = start;
+exports.app = app;
