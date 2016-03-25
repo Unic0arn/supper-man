ec98954295d0b700ad460ef55814fe472eec2eac
Fredrik Hallberg <saccer92@gmail.com>
2016-03-25 22:58:15 +0100

    Working Elastic Beanstalk

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = 8081;
 app.use(express.static(__dirname + '/../'));
-app.listen(port, function () {
+app.listen( process.env.PORT || port, function () {
   console.log('Smoothly listening on port ' +port+'!');
 });
